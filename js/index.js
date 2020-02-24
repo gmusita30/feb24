6f364db897fd35c8e61e153d6058d5f359fe15cf
gmusita30 <gabrieladrian.usita.iics@ust.edu.ph>
2020-02-24 16:13:26 +0800

    bind fuction demo

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-//apply function demo
+//bind function demo
 
 let stud1 = {name:"jdl"};
 let stud2 = {name:"pedro"};
 
-details = ["ics2609", "2itf"];
 
 let study = function(subject, section){
     console.log(`${this.name} is enrolled in ${subject}, section ${section}`);
 };
 
-study.apply(stud1, details);
+ let s = study.bind(stud2);
+s("biology", "itf");
