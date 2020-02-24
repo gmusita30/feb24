8471244de781ea464cde123e257d609bcd2bf063
gmusita30 <gabrieladrian.usita.iics@ust.edu.ph>
2020-02-24 16:53:55 +0800

    inheritance

--- js/index.js
+++ js/index.js
@@ -1,33 +1,27 @@
-//encapsulation
-class School{
-setName(name){
-    this.name = name;
+//inheritance
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    speak(language){
+        console.log(`${this.name}can speak ${language}`);
+    }
 }
 
-getName(){
-    return this.name;
+class Employee extends Person{
+    constructor(name, age, job){
+        super(name, age);
+        this.job = job;
+    }
+    work(duration){
+        console.log(`${this.name} workd ${duration} hours a day`);
+    }
 }
 
-setLocation(location){
-    this.location = location;
-}
+let p = new Person("jdl", 23);
+let w = new Employee("juan", 19, "janitor");
 
-getLocation(){
-     return this.location;
-}
-
-setPopulation(population){
-    this.population = population;
-}
-
-getPopulation(){
-    return this.population;
-}
-}
-
-let ust = new School();
-ust.setName("ust");
-ust.setLocation("manila");
-ust.setPopulation(40000);
-
-console.group(`${ust.getName()} ${ust.getLocation()} has a population of ${ust.getPopulation()}`);
+p.speak("brit");
+w.speak("yes");
+w.work("8");
